5d9604c568727104fb809d56f73f515b1f17e068
Harrison Turton <harrisonturton@gmail.com>
2021-11-13 18:06:58 +1100

    Add WIP flex column and row layout

--- core/crates/layout/src/widget.rs
+++ core/crates/layout/src/widget.rs
@@ -2,6 +2,164 @@ use super::{BoxConstraints, Layout, LayoutBox, LayoutTree, SizedLayoutBox};
 use math::{Vector2, Vector4};
 use std::fmt::Debug;
 
+// --------------------------------------------------
+// Flex
+// --------------------------------------------------
+
+#[derive(Debug)]
+pub struct Flex {
+    pub flex: Option<f32>,
+    pub child: Box<dyn Layout>,
+}
+
+// The [Flex] widget doesn't render any additional primitives. It just wraps an
+// existing widget in order to provide the `flex` property to the parent.
+impl Layout for Flex {
+    fn layout(&self, tree: &mut LayoutTree, constraints: &BoxConstraints) -> SizedLayoutBox {
+        self.child.layout(tree, constraints)
+    }
+}
+
+// --------------------------------------------------
+// Row
+// --------------------------------------------------
+
+#[derive(Default, Debug)]
+pub struct Row {
+    pub children: Vec<Flex>,
+}
+
+impl Layout for Row {
+    fn layout(&self, tree: &mut LayoutTree, constraints: &BoxConstraints) -> SizedLayoutBox {
+        // First pass calculates the sum of the heights of inflexible children
+        // and the sum of all the flex factors. These are required for the flex
+        // layout.
+        let mut sum_inflexible_width = 0.0; 
+        let mut sum_flex_factor = 0.0;
+        let child_constraints = BoxConstraints {
+            min: Vector2::new(0.0, 0.0),
+            max: Vector2::new(f32::INFINITY, constraints.max.x),
+        };
+        for widget in &self.children {
+            match widget.flex {
+                Some(flex) => {
+                    sum_flex_factor += flex;
+                },
+                None => {
+                    let sized = widget.layout(tree, &child_constraints);
+                    sum_inflexible_width += sized.size.x;
+                }
+            }
+        }
+        let free_space = constraints.max.x - sum_inflexible_width;
+        let space_per_flex = free_space / sum_flex_factor;
+
+        // Second pass performs the actual layout.
+        let mut x_pos = 0.0;
+        let mut children = vec![];
+        for widget in &self.children {
+            match widget.flex {
+                Some(flex) => {
+                    let flex_constraints = BoxConstraints {
+                        min: Vector2::new(flex * space_per_flex, 100.0),
+                        max: Vector2::new(flex * space_per_flex, constraints.max.y),
+                    };
+                    let sized = widget.layout(tree, &flex_constraints);
+                    let child = LayoutBox::from_child(sized, Vector2::new(x_pos, 0.0));
+                    let child_id = tree.insert(child);
+                    children.push(child_id);
+                    x_pos += flex * space_per_flex;
+                },
+                None => {
+                    let sized = widget.layout(tree, constraints);
+                    let child = LayoutBox::from_child(sized, Vector2::new(x_pos, 0.0));
+                    x_pos += child.rect.max.x - child.rect.min.x;
+                    let child_id = tree.insert(child);
+                    children.push(child_id);
+                },
+            }
+        }
+
+        SizedLayoutBox {
+            size: constraints.max,
+            children,
+            material: Material::Solid(Color::blue()),
+        }
+    }
+}
+
+// --------------------------------------------------
+// Column
+// --------------------------------------------------
+
+#[derive(Default, Debug)]
+pub struct Column {
+    pub children: Vec<Flex>,
+}
+
+impl Layout for Column {
+    fn layout(&self, tree: &mut LayoutTree, constraints: &BoxConstraints) -> SizedLayoutBox {
+        // First pass calculates the sum of the heights of inflexible children
+        // and the sum of all the flex factors. These are required for the flex
+        // layout.
+        let mut sum_inflexible_height = 0.0; 
+        let mut sum_flex_factor = 0.0;
+        let child_constraints = BoxConstraints {
+            min: Vector2::new(0.0, 0.0),
+            max: Vector2::new(constraints.max.x, f32::INFINITY),
+        };
+        for widget in &self.children {
+            match widget.flex {
+                Some(flex) => {
+                    sum_flex_factor += flex;
+                },
+                None => {
+                    let sized = widget.layout(tree, &child_constraints);
+                    sum_inflexible_height += sized.size.y;
+                }
+            }
+        }
+        let free_space = constraints.max.y - sum_inflexible_height;
+        let space_per_flex = free_space / sum_flex_factor;
+
+        // Second pass performs the actual layout.
+        let mut y_pos = 0.0;
+        let mut children = vec![];
+        for widget in &self.children {
+            match widget.flex {
+                Some(flex) => {
+                    let flex_constraints = BoxConstraints {
+                        min: Vector2::new(100.0, flex * space_per_flex),
+                        max: Vector2::new(constraints.max.x, flex * space_per_flex),
+                    };
+                    let sized = widget.layout(tree, &flex_constraints);
+                    let child = LayoutBox::from_child(sized, Vector2::new(0.0, y_pos));
+                    let child_id = tree.insert(child);
+                    children.push(child_id);
+                    y_pos += flex * space_per_flex;
+                },
+                None => {
+                    let sized = widget.layout(tree, constraints);
+                    let child = LayoutBox::from_child(sized, Vector2::new(0.0, y_pos));
+                    y_pos += child.rect.max.y - child.rect.min.y;
+                    let child_id = tree.insert(child);
+                    children.push(child_id);
+                },
+            }
+        }
+
+        SizedLayoutBox {
+            size: constraints.max,
+            children,
+            material: Material::Solid(Color::blue()),
+        }
+    }
+}
+
+// --------------------------------------------------
+// Stack
+// --------------------------------------------------
+
 #[derive(Debug)]
 pub struct Stack {
     pub children: Vec<Positioned>,
@@ -23,6 +181,10 @@ impl Layout for Stack {
         }
     }
 }
+
+// --------------------------------------------------
+// Positioned
+// --------------------------------------------------
 
 #[derive(Debug)]
 pub struct Positioned {
@@ -51,6 +213,10 @@ impl Layout for Positioned {
     }
 }
 
+// --------------------------------------------------
+// Container
+// --------------------------------------------------
+
 #[derive(Default, Debug)]
 pub struct Container {
     pub size: Option<Vector2>,
@@ -74,7 +240,7 @@ impl Layout for Container {
                         None => Material::None,
                     },
                 }
-            },
+            }
             None => {
                 let size = self.size.unwrap_or_default();
                 let size = size.clamp_between(constraints.min, constraints.max);
@@ -90,6 +256,10 @@ impl Layout for Container {
         }
     }
 }
+
+// --------------------------------------------------
+// Decoration
+// --------------------------------------------------
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum Material {
@@ -137,6 +307,10 @@ impl Color {
 
     pub fn blue() -> Color {
         Color::rgba(0.0, 0.0, 255.0, 255.0)
+    }
+
+    pub fn yellow() -> Color {
+        Color::rgba(255.0, 255.0, 0.0, 255.0)
     }
 
     pub fn white() -> Color {
--- core/src/app.rs
+++ core/src/app.rs
@@ -1,4 +1,4 @@
-use layout::{Color, Container, Layout, Positioned, Stack};
+use layout::{Color, Container, Layout, Positioned, Stack, Flex, Column, Row};
 use math::Vector2;
 use platform::AppDriver;
 
@@ -8,7 +8,7 @@ pub struct App {
 
 impl AppDriver for App {
     fn tick(&mut self, time: f32) -> Box<dyn Layout> {
-        self.render_boxes(time)
+        self.render_flex_row(time)
     }
 }
 
@@ -16,6 +16,86 @@ impl App {
     pub fn new() -> App {
         let position = Vector2::zero();
         App { position }
+    }
+
+    #[allow(dead_code)]
+    fn render_flex_row(&self, _: f32) -> Box<dyn Layout> {
+        Box::new(Row {
+            children: vec![
+                Flex {
+                    flex: Some(1.0),
+                    child: Box::new(Container {
+                        size: None,
+                        color: Some(Color::green()),
+                        child: None,
+                    }),
+                },
+                Flex {
+                    flex: None,
+                    child: Box::new(Container {
+                        size: Some(Vector2::new(100.0, 100.0)),
+                        color: Some(Color::red()),
+                        child: None,
+                    }),
+                },
+                Flex {
+                    flex: Some(1.0),
+                    child: Box::new(Container {
+                        size: None,
+                        color: Some(Color::yellow()),
+                        child: None,
+                    }),
+                },
+                Flex {
+                    flex: None,
+                    child: Box::new(Container {
+                        size: Some(Vector2::new(100.0, 100.0)),
+                        color: Some(Color::red()),
+                        child: None,
+                    }),
+                },
+            ],
+        })
+    }
+
+    #[allow(dead_code)]
+    fn render_flex_column(&self, _: f32) -> Box<dyn Layout> {
+        Box::new(Column {
+            children: vec![
+                Flex {
+                    flex: Some(1.0),
+                    child: Box::new(Container {
+                        size: None,
+                        color: Some(Color::green()),
+                        child: None,
+                    }),
+                },
+                Flex {
+                    flex: None,
+                    child: Box::new(Container {
+                        size: Some(Vector2::new(100.0, 100.0)),
+                        color: Some(Color::red()),
+                        child: None,
+                    }),
+                },
+                Flex {
+                    flex: Some(1.0),
+                    child: Box::new(Container {
+                        size: None,
+                        color: Some(Color::yellow()),
+                        child: None,
+                    }),
+                },
+                Flex {
+                    flex: None,
+                    child: Box::new(Container {
+                        size: Some(Vector2::new(100.0, 100.0)),
+                        color: Some(Color::red()),
+                        child: None,
+                    }),
+                },
+            ],
+        })
     }
 
     #[allow(dead_code)]
@@ -33,7 +113,7 @@ impl App {
     // 5 boxes should be placed directly next to eachother in a row. This tests
     // for bugs in how the layout algorithm positions widgets relative to their
     // parent, specifically when there are multiple independently-positioned
-    // widgets that are laid out in a row. 
+    // widgets that are laid out in a row.
     #[allow(dead_code)]
     fn render_boxes(&self, _: f32) -> Box<dyn Layout> {
         Box::new(Stack {
@@ -78,7 +158,7 @@ impl App {
                         ..Default::default()
                     }),
                 },
-            ]
+            ],
         })
     }
 
